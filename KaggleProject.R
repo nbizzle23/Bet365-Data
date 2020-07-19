66dfd20dd2cde224595a5dd535b2086d99838581
Nicholas Burke <nburke2@lakeheadu.ca>
2020-07-19 17:18:07 -0400

    Second commit

--- KaggleProject.R
+++ KaggleProject.R
@@ -30,7 +30,7 @@ head(mutate(kaggle_nba_2009_2011, final_line = h_FinalPoints-r_FinalPoints))
 #Create column for home favourite or home underdog based on for if h_Road>r_Road then print hfav ifelse print hdog
 kaggle_nba_2009_2011$favdog <- with(kaggle_nba_2009_2011, ifelse(h_oPoints > r_oPoints, "hfav", "hdog"))
 newkaggle_nba_2009_2011$favdog <-  with(newkaggle_nba_2009_2011, ifelse(pro_line > 0, "hfav", "hdog"))
-newkaggle_nba_2009_2011$homewin <-  with(newkaggle_nba_2009_2011, ifelse(h_FinalPoints > r_FinalPoints, "YES", "NO"))
+newkaggle_nba_2009_2011$homewin <-  with(newkaggle_nba_2009_2011, ifelse(h_FinalPoints > r_FinalPoints, "Win", "Loss"))
 
 View(newkaggle_nba_2009_2011$pro_line)
 View(newkaggle_nba_2009_2011$favdog)
@@ -46,7 +46,8 @@ View(newkaggle_nba_2009_2011)
 
 #Betting results df
 nbabet <- newkaggle_nba_2009_2011 %>% 
-  select(pro_line,final_line,result, home_win, favdog, homewin)
+  select(pro_line,final_line,result,home_win, favdog, homewin)
+
 #Points df
 pointsnba <- newkaggle_nba_2009_2011 %>%
   select(h_1st, h_2nd, h_3rd,h_4th,h_5th,h_o1st, h_o2nd, h_o3rd,h_o4th,h_o5th,r_1st, r_2nd, r_3rd,r_4th,r_5th,r_o1st, r_o2nd, r_o3rd,r_o4th,r_o5th, h_PointsWin, r_PointsWin, pro_line, final_line, result)
@@ -54,35 +55,79 @@ pointsnba <- newkaggle_nba_2009_2011 %>%
 #Barplot of hpwin, hplost
 library(ggplot2)
 library(ggthemes)
-ggplot(nbabet, aes(result))+ geom_bar(aes(fill=home_win)) + theme_excel()
-ggplot(nbabet, aes(result))+ geom_bar(aes(fill=favdog)) + theme_pander()
 
-ggplot(nbabet, aes(home_win)) + geom_bar(aes(fill=result)) + theme_hc()
-ggplot(nbabet, aes(home_win)) + geom_bar(aes(fill=favdog)) + theme_solarized()
+#NEED TO EDIT COLOURS FOR GRAPHS
+
+#Barplot of results filled by home wins 
+ggplot(nbabet, aes(result))+ geom_bar(aes(fill=homewin)) + theme_excel()+
+  labs(title =" Handicap Results", x = "Result", y = "Count")+
+  scale_fill_discrete(name = "Home Team",labels = c("Loss", "Win"))
+
+#Barplot of results filled by home favourite /underdog
+ggplot(nbabet, aes(result))+ geom_bar(aes(fill=favdog)) + theme_pander()+
+  labs(title =" Handicap Results", x = "Result", y = "Count")+
+  scale_fill_discrete(name = "Home",labels = c("Underdog", "Favourite"))
+
+#Barplot of Home Team Results filled by Handicap results
+ggplot(nbabet, aes(homewin)) + geom_bar(aes(fill=result)) + theme_hc()+
+  labs(title =" Home Team Results", x = "Home", y = "Count")+
+  scale_fill_discrete(name = "Handicap Result",labels = c("Loss", "Win"))
 
 
-ggplot(nbabet, aes(favdog))+ geom_bar(aes(fill=result)) + theme_foundation()
-ggplot(nbabet, aes(favdog))+ geom_bar(aes(fill=home_win)) + theme_stata()
+#Barplot of Home Team Results filled by home team favoourite/underdog
+ggplot(nbabet, aes(homewin)) + geom_bar(aes(fill=favdog)) + theme_solarized()+
+  labs(title =" Home Team Results", x = "Home", y = "Count")+
+  scale_fill_discrete(name = "Home",labels = c("Underdog", "Favourite"))
+
+#Barplot of Home Favourite/ Underdog filled by handicap results
+ggplot(nbabet, aes(favdog))+ geom_bar(aes(fill=result)) + theme_foundation()+
+  labs(title =" Home Fav/Dog", x = "Home Team", y = "Count")+
+  scale_fill_discrete(name = "Handicap Results",labels = c("Loss", "Win"))
+
+#Barplot of Home Favourite/ Underdog filled by home team results
+ggplot(nbabet, aes(favdog))+ geom_bar(aes(fill=homewin)) + theme_stata()+
+  labs(title =" Home Fav/Dog", x = "Home Team", y = "Count")+
+  scale_fill_discrete(name = "Home Team",labels = c("Loss", "Win"))
+
 
 
 #Actual count
 count(nbabet, result)
-count(nbabet, home_win)
 count(nbabet, favdog)
 count(nbabet, homewin)
+count(home_win)
+count(nbabet,result,favdog,homewin)
+
+
+
+#NEED TO EDIT COLOURS AND LABELS FOR GRAPHS
 
 #Plot density plot of pro_line and Final Line
-ggplot(nbabet,aes(pro_line))+geom_density()+theme_excel_new()
-ggplot(nbabet,aes(final_line))+geom_density()+ theme_gdocs()
+#Density plot of betting line
+ggplot(nbabet,aes(pro_line))+geom_density(fill="red")+theme_gdocs()+
+  labs(title ="Pro Line", x = "Pro Line", y = "Density")
+
+#Density plot of final line
+ggplot(nbabet,aes(final_line))+geom_density(fill="blue")+ theme_gdocs()+
+  labs(title ="Final Line", x = "Pro Line", y = "Density")
 
 
 #Plot pro line and final line
-qplot(x=pro_line, y=final_line, data = nbabet, colour=result) + theme_dark()
-ggplot(nbabet, aes(x=pro_line, y=final_line))+geom_point(aes(color=result)) + theme_fivethirtyeight()
+# Scatterplot of pro vs final line(Spread) filled with handicap result
+qplot(x=pro_line, y=final_line, data = nbabet, colour=result) + theme_dark()+
+  labs(title ="Distribution of Spread", x = "Pro Line", y = "Final Line")+
+  scale_fill_discrete(name = "Handicap",labels = c("Loss", "Win"))
 
-#Combined plot #Learn how to add colour
+#This plot does not have label axises
+ggplot(nbabet, aes(x=pro_line, y=final_line))+geom_point(aes(color=result)) + theme_fivethirtyeight()+
+  labs(title ="Distribution of Spread", x = "Pro Line", y = "Final Line")+
+  scale_fill_discrete(name = "Handicap",labels = c("Loss", "Win"))
+
+#Combined plots
 library(GGally)
-ggpairs(nbabet)
+#Edit different graphs
+ggpairs(nbabet, mapping = aes(color= result)) + theme_solarized_2()
+
 str(ggpairs(nbabet))
 
 #Structure of dfs
--- README.md
+++
@@ -1,2 +0,0 @@
-# NBA_data-with-bet365-2009-2011-
-NBA_data with bet365(2009-2011)
